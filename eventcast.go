37013cae3df2dd88784ae8ca07eef19993b02889
Albert Tedja <albert@alberttedja.com>
2015-11-25 00:33:44 -0800

    Fixed doc and renamed key to event

--- eventcast.go
+++ eventcast.go
@@ -11,37 +11,38 @@ var channels = struct {
 	list: make(map[string][]chan interface{}),
 }
 
-// Broadcast, unblock all channels.
+// Broadcast an event.
 //
-func Broadcast(key string) {
-	BroadcastWithValue(key, nil)
+func Broadcast(event string) {
+	BroadcastWithValue(event, nil)
 }
 
-// Broadcast with a value.
+// Broadcast an event with a value.
 //
-func BroadcastWithValue(key string, value interface{}) {
+func BroadcastWithValue(event string, value interface{}) {
 	channels.Lock()
 	defer channels.Unlock()
-	chans := channels.list[key]
+	chans := channels.list[event]
 	if chans != nil {
 		for _, ch := range chans {
 			ch <- value
 			close(ch)
 		}
-		channels.list[key] = nil
+		channels.list[event] = nil
 	}
 }
 
 // Listener. Retrieve a channel that listens to a broadcast event.
-// This method creates a one-time use channel, everytime, and a Broadcast() will close and remove them.
+// This method creates a one-time use channel, everytime, and a Broadcast()
+// will push data and close them.
 //
-func Listen(key string) chan interface{} {
+func Listen(event string) chan interface{} {
 	channels.Lock()
 	defer channels.Unlock()
 	out := make(chan interface{}, 1)
-	if channels.list[key] == nil {
-		channels.list[key] = make([]chan interface{}, 0, 8)
+	if channels.list[event] == nil {
+		channels.list[event] = make([]chan interface{}, 0, 8)
 	}
-	channels.list[key] = append(channels.list[key], out)
+	channels.list[event] = append(channels.list[event], out)
 	return out
 }
